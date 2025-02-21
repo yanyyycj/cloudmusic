b59ab28351fda82bb2f698b9fb329473d5159544
ycj <2954322679@qq.com>
2025-02-21 20:32:33 +0800

    创建引导界面

--- app/src/main/java/com/ixuea/courses/mymusic/component/guide/activity/GuideActivity.java
+++ app/src/main/java/com/ixuea/courses/mymusic/component/guide/activity/GuideActivity.java
@@ -1,10 +1,36 @@
 package com.ixuea.courses.mymusic.component.guide.activity;
 
+import android.view.View;
+
+import com.ixuea.courses.mymusic.MainActivity;
+import com.ixuea.courses.mymusic.R;
 import com.ixuea.courses.mymusic.activity.BaseViewModelActivity;
 import com.ixuea.courses.mymusic.databinding.ActivityGuideBinding;
 
+public class GuideActivity extends BaseViewModelActivity<ActivityGuideBinding> implements View.OnClickListener {
 
-public class GuideActivity extends BaseViewModelActivity<ActivityGuideBinding> {
+    @Override
+    protected void initListeners() {
+        super.initListeners();
+        binding.loginOrRegister.setOnClickListener(this);
+        binding.experienceNow.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.login_or_register) {
+            setShowGuide();
+
+        } else {
+            startActivityAfterThis(MainActivity.class);
+            setShowGuide();
+        }
+
+    }
+
+    private void setShowGuide() {
+
+    }
 
 }
